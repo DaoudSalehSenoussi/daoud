4bb10d325134c32e012d0df0faf0f9c410e89d21
DaoudSalehSenoussi <daoudsaley66100111@gmail.com>
2020-08-28 22:47:42 +0300

    Bootrap projesi

---
+++ app/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+
+    protected $fillable = [
+    	'name', 'photo', 'price', 'created_by', 'updated_by','deleted_by' //area available
+    ];
+
+    public function user()
+    {
+    	return $this->hasMany('App\User','id','created_by'); 
+    }
+
+}
